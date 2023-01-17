f1cef040f7c51dac475369c7bafb0cdcb09de2f5
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-17 14:38:17 +0100

    Aggiunta tabella Categoria nel DataBase

--- LaMiaPizzeriaEFRelazione1n/Migrations/20230117133632_relazione1nPizzeria.Designer.cs
+++ LaMiaPizzeriaEFRelazione1n/Migrations/20230117133632_relazione1nPizzeria.Designer.cs
@@ -4,6 +4,7 @@ using LaMiaPizzeriaEFRelazione1n.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LaMiaPizzeriaEFRelazione1n.Migrations
 {
     [DbContext(typeof(PizzeriaContext))]
-    partial class PizzeriaContextModelSnapshot : ModelSnapshot
+    [Migration("20230117133632_relazione1nPizzeria")]
+    partial class relazione1nPizzeria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
