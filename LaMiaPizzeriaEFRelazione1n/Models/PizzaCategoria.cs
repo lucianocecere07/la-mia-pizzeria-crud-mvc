b9ce1edbfed79728ba46f7b0fd6ca1ffe5bb9531
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-17 14:57:08 +0100

    creazione modello PizzaCategoria

---
+++ LaMiaPizzeriaEFRelazione1n/Models/PizzaCategoria.cs
@@ -0,0 +1,8 @@
+﻿namespace LaMiaPizzeriaEFRelazione1n.Models
+{
+    public class PizzaCategoria
+    {
+        public Pizza Pizza { get; set; }
+        public List<Categoria> Categoria { get; set; }
+    }
+}
